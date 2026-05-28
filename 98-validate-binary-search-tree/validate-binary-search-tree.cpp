a757fe12b57ff67e7665a4bbea04923163ce1ec1
manas-1511 <manas.p1511@gmail.com>
2026-05-28 15:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (94.63%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,42 +11,15 @@
  */
 class Solution {
 public:
-    TreeNode* getleftmost(TreeNode* root){
-        if(root== NULL) return NULL;
-        if(root->left == NULL) return root;
-        return getleftmost(root->left);
-
-    }
-
-    TreeNode* getrightmost(TreeNode* root){
-        if(root== NULL) return NULL;
-        if(root->right == NULL) return root;
-        return getrightmost(root->right);
-    }
-
     bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN , LONG_MAX);
+    }
+
+
+    bool helper( TreeNode* root , long long  MIN , long long  MAX){
         if(root == NULL) return true;
-        TreeNode* rightsubtree = getleftmost(root->right);
-        TreeNode* leftsubtree = getrightmost(root->left);
-        bool r = false ;
-        bool l = false ;
-
-        if(rightsubtree == NULL) r = true ;
-        else{
-            if(rightsubtree->val > root->val)r = true;
-        }
-        if(leftsubtree == NULL) l = true;
-        else{
-            if(leftsubtree->val < root->val) l = true;
-
-        }
-
-        bool a = isValidBST(root->left);
-        bool b = isValidBST(root->right);
-
-        return r && l && a && b;
-
-
-
+        if(root->val <= MIN || root->val >= MAX) return  false ;
+        
+        return helper(root->left , MIN , root->val) && helper(root->right , root->val , MAX);
     }
 };
