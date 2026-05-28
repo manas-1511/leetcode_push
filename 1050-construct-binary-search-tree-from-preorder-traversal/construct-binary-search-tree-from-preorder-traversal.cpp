316c7836567ebe1dc41ba59c146ea2a0c06c19a1
manas-1511 <manas.p1511@gmail.com>
2026-05-29 00:47:45 +0530

    Time: 3 ms (17.73%) | Memory: 16.4 MB (45.54%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,54 +11,19 @@
  */
 class Solution {
 public:
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        if(preorder.size() == 0 ) return NULL;
-        TreeNode* root = new TreeNode(preorder[0]);
-        if(preorder.size() == 1){
-            
-            return root;
-        }
+    TreeNode* bstFromPreorder(vector<int>& A) {
+        int i = 0 ;
+        return build(A, i , INT_MAX);
+    }
 
-        int leftindex = -1;
-        int rightindex = -1;
-        if(preorder[0] > preorder[1]) leftindex = 1;
+    TreeNode* build(vector<int> &A , int& i , int bound){
+        if( i == A.size() || A[i] >= bound) return NULL;
+        TreeNode* root = new TreeNode(A[i]);
+        i++;
+        root->left = build(A, i , root->val);
+        root->right = build(A, i , bound);
 
-        for(int i = 1 ; i < preorder.size() ; i ++){
-            if(preorder[0] < preorder[i]) {
-                rightindex = i;
-                break;
-            }
-        }
-        vector<int> leftv;
-        vector<int> rightv;
-        if(leftindex != -1){
-            if(rightindex == -1){
-                leftv =  vector<int>(preorder.begin() +1 , preorder.end());
-                
-            }
-            else{
-                leftv = vector<int>(preorder.begin() +1 , preorder.begin() + rightindex);
-            }
-            
-        }
-
-        if(rightindex != -1){
-            rightv = vector<int>(preorder.begin() +rightindex  , preorder.end());
-            
-        }
-
-        TreeNode* leftsub = bstFromPreorder(leftv);
-        TreeNode* rightsub = bstFromPreorder(rightv);
-        root->left = leftsub;
-        root->right = rightsub;
         return root;
-        
-        
 
-
-
-
-    
-        
     }
 };
