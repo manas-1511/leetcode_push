4cdde2a8e8aff6d499565adcaa59e8074968ddb1
manas-1511 <manas.p1511@gmail.com>
2026-05-31 04:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (95.10%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -10,46 +10,39 @@
  * };
  */
 class Solution {
-public:
-    void traversal(TreeNode* root , vector<TreeNode*> &v){
-        if( root == NULL) return;
-        traversal(root->left , v);
-        v.push_back(root);
-        traversal(root->right , v);
-    }
-    void recoverTree(TreeNode* root) {
+private: 
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+
+    void inorder(TreeNode* root){
         if(root == NULL) return;
-        if(root->left == NULL && root->right == NULL) return;
-         vector<TreeNode*> v ;
-        traversal(root , v);
-        TreeNode* first = NULL;
-        TreeNode* middle = NULL;
-        TreeNode* last = NULL;
-        for(int i = 1 ; i < v.size() ; i ++){
-            if(v[i]->val <= v[i-1]->val){
-                if(first == NULL) {
-                    first = v[i-1];
-                    middle = v[i];
-                }
-
-                else last = v[i];
+        inorder(root->left);
+        if(prev != NULL && root->val <= prev->val ){
+            if(first == NULL){
+                first = prev ; 
+                middle = root;
             }
+
+            else{
+                last = root;
+            }
+
+
         }
 
-        if(last == NULL){
-            int temp = first->val;
-            first->val = middle->val;
-            middle->val = temp;
-        }
+        prev = root;
+        inorder(root->right);
 
-        else{
-            int temp = first->val;
-            first->val = last->val;
-            last->val = temp;
-        }
+    }
 
-        return ;
-
-
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = NULL;
+        inorder(root);
+        if(first && last) swap(first->val , last->val);
+        else swap(first->val , middle->val);
     }
 };
