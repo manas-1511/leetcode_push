d7cf2450c97c658151605c337c2cf68efb9d967a
manas-1511 <manas.p1511@gmail.com>
2026-05-30 17:26:28 +0530

    Time: 1 ms (53.11%) | Memory: 12.8 MB (36.36%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int , int> mpp; 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        for(int i = 0 ; i < nums2.size() ; i ++){
-            mpp[nums2[i]] = i;
-        }
-        vector<int> res(n2);
-
+        unordered_map<int , int>mpp;
         stack<int> st;
-        for(int i = n2-1 ; i>=0; i--){
-            while(!st.empty() && nums2[i] >= st.top()){
+        int n = nums2.size();
+        for(int i = n-1 ; i>= 0 ; i --){
+            int copy = nums2[i];
+            mpp[nums2[i]] = i;
+            while(!st.empty() && st.top() <= copy){
                 st.pop();
             }
 
-            if(st.empty()) res[i] = -1;
-            else res[i] = st.top();
-
-            st.push(nums2[i]);
+            if(st.empty()) nums2[i] = -1;
+            else nums2[i] = st.top();
+            st.push(copy);
         }
 
-        for(int i = 0 ; i< n1 ; i ++){
-            nums1[i] = res[mpp[nums1[i]]];
+        for(int i = 0 ; i < nums1.size() ; i++){
+            nums1[i] = nums2[mpp[nums1[i]]];
         }
 
         return nums1;
