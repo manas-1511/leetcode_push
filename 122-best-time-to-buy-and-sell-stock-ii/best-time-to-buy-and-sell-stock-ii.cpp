97d23fab7be37f0d00ff237aa37afc32677cac9f
manas-1511 <manas.p1511@gmail.com>
2026-06-11 02:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (60.61%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //space optimization
         int n = prices.size();
-        vector<vector<int>> dp(2 , vector<int>(n+1,0));
+        int prev0 = 0;
+        int prev1 = 0;
         
             for(int j = n-1 ; j >= 0; j--){
-                dp[0][j] = max( prices[j] + dp[1][j+1] , dp[0][j+1]);
-                dp[1][j] = max(-prices[j] + dp[0][j+1] , dp[1][j+1]);
+                int temp0 = prev0;
+                prev0 = max( prices[j] + prev1 , prev0);
+                prev1 = max(-prices[j] + temp0 , prev1);
 
             }
 
-            return dp[1][0];
+            return prev1;
         
 
 
