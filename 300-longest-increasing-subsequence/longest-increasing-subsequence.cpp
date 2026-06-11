e9d91a9045756ea5584a6928b010f8862df0528e
manas-1511 <manas.p1511@gmail.com>
2026-06-11 23:36:28 +0530

    Time: 64 ms (56.28%) | Memory: 14.4 MB (44.24%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-//Tabulation
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-         vector<int> next(n+1 , 0);
-        //coulmns are from -1 to n-1 (prev_index)
-        //rows = 0 to n (0 based index) but nth row just to maintain the edge values
-        //pi = prev_idx
-
-        for(int i = n-1 ; i>= 0 ; i--){
-             vector<int> curr(n+1 , 0);
-            for(int pi = i-1 ; pi>=-1 ; pi--){
-                //index = i+1 , prev_index = same pi
-                //because -1 is 0th column in dp therefore dp[x][pi+1] represents it
-                int notTake = next[pi+1];
-
-                int Take = 0;
-                if(pi == -1 || nums[pi] < nums[i]) Take = 1+next[i+1];
-                curr[pi+1] = max(notTake , Take);
-
+        vector<int> dp(n , 1);
+        int ans  = 1;
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j <= i-1 ; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i] , 1+dp[j]);
+                }
             }
 
-            next = curr;
+            ans = max(ans , dp[i]);
+
+
         }
 
-        return next[-1+1];
+        return ans;
+
+        
     }
 };
