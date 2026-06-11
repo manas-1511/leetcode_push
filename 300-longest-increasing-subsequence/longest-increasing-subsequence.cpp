77e8bd48c3068d3b09123e882f671aeb2557f6d4
manas-1511 <manas.p1511@gmail.com>
2026-06-11 23:03:52 +0530

    Time: 279 ms (29.08%) | Memory: 296.6 MB (5.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -6,34 +6,22 @@ public:
         int n = nums.size();
         vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
         //coulmns are from -1 to n-1 (prev_index)
-        //rows = 0 to n (1 based index)
-        for(int i = 1 ; i<= n ; i++){
-            dp[i][0] = 1;
-        }
+        //rows = 0 to n (0 based index) but nth row just to maintain the edge values
+        //pi = prev_idx
 
-        for(int i =1 ; i <= n; i++){
-            for(int j = 0 ; j <= n ; j++){
-                
-                    //prev_index = -1
-                    int notTake = dp[i-1][j];
-                    int Take = 0 ;
-                     if(j==0 || nums[i-1] < nums[j-1]) Take = 1+dp[i-1][i];
-                    dp[i][j] = max(Take , notTake);
-                
+        for(int i = n-1 ; i>= 0 ; i--){
+            for(int pi = i-1 ; pi>=-1 ; pi--){
+                //index = i+1 , prev_index = same pi
+                //because -1 is 0th column in dp therefore dp[x][pi+1] represents it
+                int notTake = dp[i+1][pi+1];
 
-
-
-
+                int Take = 0;
+                if(pi == -1 || nums[pi] < nums[i]) Take = 1+dp[i+1][i+1];
+                dp[i][pi+1] = max(notTake , Take);
 
             }
-            
         }
 
-        int ans = INT_MIN;
-        for(int i = 0 ; i <= n ; i++){
-            ans = max(ans , dp[n][i]);
-        }
-
-        return ans;
+        return dp[0][-1+1];
     }
 };
