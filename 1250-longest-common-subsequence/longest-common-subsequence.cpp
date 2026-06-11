a6177b0ed2240e192a1615594e328bcefda2ca42
manas-1511 <manas.p1511@gmail.com>
2026-06-11 19:44:13 +0530

    Time: 28 ms (40.40%) | Memory: 27.4 MB (67.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -6,9 +6,8 @@ public:
         int m = text1.size();
         int n = text2.size();
         
-        vector<vector<int>> dp(m+1 , vector<int>(n+1 ,-1));
-        for(int i = 0 ; i <= m ; i++) dp[i][0] = 0 ; 
-        for(int i = 0 ; i <= n ; i++) dp[0][i] = 0 ; 
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 ,0));
+        
         for(int i = 1 ; i <= m ; i ++){
             for(int j = 1 ;j <= n ; j ++){
                 if(text1[i-1] == text2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
