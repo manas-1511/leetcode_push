40adbeaca9f58926e05812272735a85a8ca18c63
manas-1511 <manas.p1511@gmail.com>
2026-06-11 23:11:40 +0530

    Time: 272 ms (30.32%) | Memory: 294.8 MB (27.99%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,24 +4,27 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
+         vector<int> next(n+1 , 0);
         //coulmns are from -1 to n-1 (prev_index)
         //rows = 0 to n (0 based index) but nth row just to maintain the edge values
         //pi = prev_idx
 
         for(int i = n-1 ; i>= 0 ; i--){
+             vector<int> curr(n+1 , 0);
             for(int pi = i-1 ; pi>=-1 ; pi--){
                 //index = i+1 , prev_index = same pi
                 //because -1 is 0th column in dp therefore dp[x][pi+1] represents it
-                int notTake = dp[i+1][pi+1];
+                int notTake = next[pi+1];
 
                 int Take = 0;
-                if(pi == -1 || nums[pi] < nums[i]) Take = 1+dp[i+1][i+1];
-                dp[i][pi+1] = max(notTake , Take);
+                if(pi == -1 || nums[pi] < nums[i]) Take = 1+next[i+1];
+                curr[pi+1] = max(notTake , Take);
 
             }
+
+            next = curr;
         }
 
-        return dp[0][-1+1];
+        return next[-1+1];
     }
 };
