6cf22108411764928a93d913f95058136bb3ded3
manas-1511 <manas.p1511@gmail.com>
2026-06-12 14:44:05 +0530

    Time: 8 ms (80.84%) | Memory: 190.4 MB (94.76%) - LeetSync

--- 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.cpp
+++ 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.cpp
@@ -1,47 +1,29 @@
 class Solution {
-    private: int n = 0;
-          //taken = 1 ;
-          //notTaken = 0 ;
-
 public:
-    // long long f(int i , int taken , vector<int>& nums, string& s , vector<vector<long long>>& dp){
-    //     if(i>= n) return 0;
-    //     if(dp[i][taken] != -1) return dp[i][taken];
-    //     long long  ans = f(i+1 , 0 , nums , s , dp);
-    //     if(s[i] == '1' && !taken){
-    //         ans = max( ans , 1LL*nums[i] + f(i+1 , 0 , nums , s ,dp));
-    //     }
-
-    //     if(i+1 <n && s[i+1] == '1'){
-    //         ans = max(ans  , 1LL*nums[i] + f(i+1 , 1 , nums , s ,dp));
-    //     }
-
-    //     return dp[i][taken] = ans;
-
-        
-    // }
-
-//tabulation
     long long maxTotal(vector<int>& nums, string s) {
-        n = nums.size();
-        vector<vector<long long>> dp(n+1, vector<long long>(2 , 0));
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int j = 0 ; j<= 1; j++){
-                long long  a = dp[i+1][0];
-                long long  b = 0 ;
-                if(s[i] == '1' && j==0){
-                    b = 1LL*nums[i] + dp[i+1][0];
-                }
-                long long c = 0 ;
-                if(i+1 < n && s[i+1] == '1'){
-                    c =  1LL*nums[i] + dp[i+1][1];
+        int i = 0 ;
+        int n = nums.size();
+        long long ans = 0 ;
+        while(i< n && s[i] == '1'){
+            ans += nums[i];
+            i++;
+        }
+
+        for(i;  i <n ; i++){
+            if(s[i] == '1'){
+                long long csm = 0 ;
+                csm += 1LL*nums[i-1];
+                long long mn = 1LL*nums[i-1];
+                while(i < n && s[i] == '1'){
+                    csm += 1LL*nums[i];
+                    mn = min(mn , 1LL*nums[i]);
+                    i++;
                 }
 
-                dp[i][j] = max(a ,max(b ,c));
-                
+                ans+= csm-mn;
             }
         }
 
-        return dp[0][0];
+        return ans;
     }
 };
