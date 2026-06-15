ab53641482a85319553bc489717e43a86b9832b2
manas-1511 <manas.p1511@gmail.com>
2026-06-16 01:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (98.18%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-//brute force 
-    //tC : n! * n 
-    //SC: O(n) + O(n) one is for map and the other for the curr array to be stored
-    void helper(vector<int>& nums , vector<int>& curr , vector<int>& visited  ,vector<vector<int>>& ans ){
-        if(curr.size() == nums.size()){
-            ans.push_back(curr);
-        }
-        int n = nums.size();
+//optimized approach 
 
-        for(int i = 0 ; i < n ; i ++){
-            if(!visited[i]){
-                visited[i] = 1;
-                curr.push_back(nums[i]);
-                helper(nums , curr , visited , ans );
-                visited[i] = 0; 
-                curr.pop_back();
-            }
+
+//TC:n!*n because for every step iam iterating all the idices to the right of the current pointer and there are n! combinations
+//SC: o(1) if recursion stack spaces are ignored 
+
+    void helper(int idx  , vector<vector<int>> &ans , vector<int>& nums){
+        if(idx== nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = idx ; i  <nums.size() ; i ++ ){
+            swap(nums[idx] ,nums[i] );
+            helper(idx+1 , ans , nums);
+            swap(nums[idx] , nums[i]);
         }
 
     }
     vector<vector<int>> permute(vector<int>& nums) {
-         vector<vector<int>> ans ; 
-        vector<int> curr;
-        vector<int> visited(nums.size() , false);
-        helper(nums , curr , visited  ,ans);
+        vector<vector<int>> ans;
+        vector<int>curr;
+        helper( 0  , ans  , nums);
         return ans;
     }
 };
