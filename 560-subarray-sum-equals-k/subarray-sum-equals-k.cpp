d9bcf422c4cb60cc906da2c5b1ce907284571f52
manas-1511 <manas.p1511@gmail.com>
2026-06-17 16:50:47 +0530

    Time: 39 ms (71.72%) | Memory: 45.5 MB (40.52%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int , int> prefixSumMap;
+        prefixSumMap[0] = 1;
+        int sum = 0 ;
+        int n = nums.size();
+        int cnt = 0;
+        for(int i = 0 ; i < n ; i ++){
+            sum+=nums[i];
+
+
+                int rem = sum-k;
+                if(prefixSumMap.find(rem) != prefixSumMap.end()){
+                    // int index = prefixSumMap[rem];
+                    cnt+=prefixSumMap[rem];
+                }
+
+            //its not present in the map then add it 
+            
+                prefixSumMap[sum]++;
+            
+        }
+
+        return cnt;
+    }
+};
