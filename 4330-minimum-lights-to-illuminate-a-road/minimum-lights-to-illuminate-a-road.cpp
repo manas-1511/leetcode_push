f2b1c3761701592e41431fad0745a2f7cecb57a5
manas-1511 <manas.p1511@gmail.com>
2026-06-21 03:39:03 +0530

    Time: 27 ms (50.00%) | Memory: 277.8 MB (70.00%) - LeetSync

--- 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
+++ 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
@@ -1,55 +1,33 @@
 class Solution {
 public:
     int minLights(vector<int>& lights) {
-
         int n = lights.size();
-        vector<vector<int>> limits;
-        for(int i = 0 ; i <n ; i++){
-            int v = lights[i];
-            if(v== 0) continue;
-            limits.push_back({max(0 , i-v) , min(n-1 , i+v)});
-        }
-    if(limits.size() == 0 ) return (n+2)/3;
+        vector<int> diff(n , 0);
+        for(int i = 0 ; i < n ; i++){
+            int v = lights[i]; 
+            if(v>0){
+                int l = i-v;
+                int r = min( n-1,i+v);
+                diff[max(l , 0)]++;
+                if(r+1<n) diff[r+1]--;
 
-        
-        sort(limits.begin() , limits.end());
-
-        
-        int cnt = 0;//cnt = the total number of darks positions 
-        
-        
-       int last_covered = -1;
-       //took -1 because we want to check the drak gaps at the starting
-       //lets say [1,3] is the first limits lit then 1-(-1)-1 = 1 (that means 1 dark position)
-
-        for(int i = 0; i < limits.size(); i++){
-            if(limits[i][0] > last_covered+1){
-                int gap = limits[i][0]-last_covered-1;
-                cnt+=(gap+2)/3;
             }
-
-                //in any of the case you have to update the last covered
-                last_covered = max(last_covered , limits[i][1]);
         }
 
-            //for trailling gaps 
-            int final_dark_gaps = n-last_covered-1;
-            if(final_dark_gaps > 0 ) {
-                cnt+=(final_dark_gaps+2)/3;
+        for(int i =1 ; i < n ; i++){
+            diff[i] = diff[i]+diff[i-1];
+        }
+int cnt = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(diff[i] == 0){
+                cnt++;
+                if(i+1 < n) diff[i+1]++;
+                if(i+2 < n) diff[i+2]++; 
             }
+        }
 
-            return cnt;
-            
-
-
+        return cnt;
 
             
-
-
-
-
-        
-
-        
     }
 };
