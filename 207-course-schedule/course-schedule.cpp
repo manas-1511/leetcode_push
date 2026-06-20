46607e8cad17203cf444fe73d00defc4ec187d95
manas-1511 <manas.p1511@gmail.com>
2026-06-21 02:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (54.43%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    //this was using the pathvis and vis 
-    bool checkdfs(int i ,vector<vector<int>>& adj , vector<int>& vis , vector<int> pathvis ){
-        vis[i] = 1;
-        pathvis[i] = 1;
-        for(auto&it : adj[i]){
-            if(!vis[it]){
-                if(checkdfs(it , adj , vis , pathvis)){
-                    return true;
-                    //yes its a cycle 
-                }
-            }
 
-            else{
-                //that indicates that the node is visited and also it is 
-                //path visited that means the current path in which i am travelling in 
-                //has already encountered this node and now i am in a cycle
-                if(pathvis[it]) return true;
-            }
-        }
-
-        pathvis[i] = 0;
-        return false;
-
-    }
-//check if the directed graph is the cyclic one
+   
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        //bfs
         vector<vector<int>> adj(n);
+        vector<int> indegree(n);
         for(auto& e : prerequisites){
             adj[e[1]].push_back(e[0]);
+            indegree[e[0]]++;
         }
         vector<int> vis(n , 0);
-        vector<int> pathvis(n , 0);
 
-        for(int i = 0 ; i < n ; i++){
-            if(!vis[i]){
-                bool flag = checkdfs(i , adj , vis , pathvis);
-                //if the flag = true that means it is a directed graph so have to return false to say that the courses can cannot be ever completed by the student 
-                if(flag) return false;
+        
+
+         queue<int> q;
+         int cnt = 0;
+         for(int i = 0 ; i < n ; i ++){
+            if(indegree[i] == 0 ) {
+                q.push(i);
+
             }
-        }
+         }
 
-        return true;
+         while( !q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for(auto& it : adj[node]){
+                if(!vis[it]){
+                    indegree[it]--;
+                    if(indegree[it] == 0)q.push(it);
+                    
+                }
+            }
+         }
+
+         if(cnt == n) return true;
+         return false;
+ 
+
+
     }
 };
