c83c125af9a5e5dfb68fb220db44791cb19a98e3
manas-1511 <manas.p1511@gmail.com>
2026-06-21 21:15:55 +0530

    Time: 5 ms (79.81%) | Memory: 22.8 MB (78.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -10,7 +10,9 @@ public:
             int mid = (low+high)/2;
             long long t = 0;
             for(int i = 0 ; i < n ; i++){
-                t+=1LL*ceil((double)piles[i]/mid);
+                // t+=1LL*ceil((double)piles[i]/mid);
+                t += (piles[i]-1)/mid + 1;
+                // or use t = (piles[i]-1)/mid + 1
             }
             //if i am taking too much time to eat then increase the rate k
             if(t > h) low = mid+1;
