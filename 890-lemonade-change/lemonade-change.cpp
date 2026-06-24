2a370259e57325b03d38d4628b678ea25cf515aa
manas-1511 <manas.p1511@gmail.com>
2026-06-25 05:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (44.62%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+
+        int cnt5 = 0 ;
+        int cnt10  = 0 ;
+        int cnt20 = 0;
+        for(int i = 0 ; i < bills.size() ; i++){
+            if(bills[i] == 5) cnt5++;
+            else if(bills[i] == 10){
+                if(cnt5 > 0){
+                    cnt5--;
+                    cnt10++;
+                }
+
+                else return false;
+            }
+
+            else{
+
+
+                 if(cnt5>=1 && cnt10>=1){
+                    cnt5--;
+                    cnt10--;
+                    cnt20++;
+                }
+
+                else if(cnt5 >= 3){
+                    cnt5-=3;
+                    cnt20++;
+                }
+
+                else return false;
+            }
+        }
+
+
+        return true;
+    }
+};
