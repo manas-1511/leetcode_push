57b6b59749aa94e6245e67e2c0f1bb75b1eea586
manas-1511 <manas.p1511@gmail.com>
2026-06-28 02:29:44 +0530

    Time: 1 ms (13.73%) | Memory: 10.8 MB (7.71%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
+    //dp
     bool checkValidString(string s) {
-        //most optimized solution O(n)
-        //dp memo O(n^2)
-        //recursion O(3^n) because of three posibilities of * -1 , 0 , +1
+        //idx 0 to n
+        // cnt 0 to n-1;
+        int n = s.size();
 
-        int min  = 0 ; 
-        int max = 0;
-        //min and max are the range on cnt 
-        //( --> cnt++     ) ---> cnt--
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == '('){
-                min++;
-                max++;
+        //dp[i][cnt] = dp[i+1][cnt] || dp[i+1][cnt+1] || dp[i+1][cnt-1];
+        vector<vector<int>>dp( n+1 , vector<int>(n+2 , 0));
+        dp[n][0] = 1 ;
+
+        for(int i = n-1 ; i >= 0 ; i-- ){
+            for(int j = n-1; j >= 0 ; j--){
+                char c = s[i];
+                if(c == '(' && j+1 <= n) dp[i][j] = dp[i+1][j+1];
+                if(c == ')' &&  j -1 >=0 ) dp[i][j] = dp[i+1][j-1];
+                if(c == '*'){
+                    dp[i][j] = dp[i+1][j] ;
+                    if(j+1 <= n) dp[i][j] = dp[i][j] || dp[i+1][j+1];
+                    if(j-1 >= 0 ) dp[i][j] = dp[i][j] || dp[i+1][j-1];
+                }
             }
-
-            else if(s[i] == ')'){
-                 min--;
-                max--;
-            }
-
-            else{
-                min--;
-                max++;
-            }
-            
-            //making min = 0 if neg because max may be +ve
-            if(min < 0 ) min = 0;
-            //but if max also goes -ve then there is no way it's valid 
-
-            if(max<0) return false;
         }
 
-        if(min == 0 ) return true;
-        return false;
-
+        return dp[0][0];
     }
-    
 };
