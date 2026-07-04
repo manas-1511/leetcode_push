aff95158c580cfedb0137614065cf45a3d04625e
manas-1511 <manas.p1511@gmail.com>
2026-07-04 11:17:55 +0530

    Time: 51 ms (83.68%) | Memory: 134.2 MB (59.76%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -6,36 +6,39 @@ public:
             adj[it[0]].push_back({it[1] , it[2]});
             adj[it[1]].push_back({it[0] , it[2]});
         }
-        //dis , node
-        //dis = here is the min road length encountered till now
-        // queue<pair<int , int>> q;
-        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq;
-        vector<int> dis(n+1 , INT_MAX);
-        pq.push({INT_MAX , 1});
-        while(!pq.empty()){
-            auto [d , node] = pq.top();
-            pq.pop();
+        
+        queue<int> q;
 
-            //we should use pq to reduce the inefficient traversal through the roads 
+        //just bfs and mark the least lenght road
+        
+        vector<int> vis(n+1 , 0);
+        q.push(1);
+        vis[1] = 1;
+        int ans = INT_MAX;
+        while(!q.empty()){
 
+            int node = q.front();
+            q.pop();
+
+         
             for(auto& it : adj[node]){
                 int newNode = it.first;
                 int w = it.second;
 
-                //even if I encounter n , I will not stop because we can visit 
-                //each road multiple times , and 1 and can be visited multiple times 
-
-                int min_road_till_now = min(w , d);
-                if(min_road_till_now < dis[newNode]){
-                    dis[newNode] = min_road_till_now;
-                    pq.push({dis[newNode] , newNode});
+                ans = min(ans , w);
+                if(!vis[newNode]){
+                    vis[newNode] = 1;
+                    q.push(newNode);
                 }
+
+            
+                
 
             }
 
         }
 
-        return dis[n];
+        return ans;
 
 
 
