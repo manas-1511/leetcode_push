1de10bcfd5e66c246c9d9e47f18cf1fafc926c88
manas-1511 <manas.p1511@gmail.com>
2026-07-06 10:09:35 +0530

    Time: 60 ms (32.57%) | Memory: 94.1 MB (39.55%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    static bool compare(vector<int>& a , vector<int>& b){
-        
-        if(a[1] != b[1]) return  (a[1] < b[1]);
-        return a[0] < b[0];
+    static bool compare(vector<int>& a  , vector<int>& b){
+        if(a[1] == b[1]) return a[0] > b[0];
+        return (a[1] < b[1]);
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //reverse of N-meeting problem just framed differently 
-        sort(intervals.begin() , intervals.end(), compare);
-        int ans = 0 ;
-        int pre_end = INT_MIN;
-        for(auto &it : intervals){
-            if(pre_end <= it[0]){
-                ans++;
-                pre_end = it[1];
-            }
+        sort(intervals.begin() ,intervals.end(), compare);
+        int end_prev = INT_MIN;
+        int cnt = 0 ;
+        for(auto& in : intervals){
+            int st = in[0];
+            int en = in[1];
 
+            if(end_prev<=st){
+                cnt++;
+                end_prev = en;
+            }
         }
 
-        return intervals.size() - ans;
+        return intervals.size() - cnt;
+
     }
 };
