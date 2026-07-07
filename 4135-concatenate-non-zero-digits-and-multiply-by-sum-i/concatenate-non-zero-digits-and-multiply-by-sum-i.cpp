1c59a9b70c6bbb429d1f8288cd165a50893ddc3b
manas-1511 <manas.p1511@gmail.com>
2026-07-07 09:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (89.09%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -3,19 +3,21 @@ public:
     long long sumAndMultiply(int n) {
         long long  ans = 0 ;
         long long  sum = 0 ;
-        stack<long long> st ;
+        // stack<long long> st ;
+        long long place = 1;
         while(n>0){
             long long  d =n%10;
             n = n/10;
             if(d == 0) continue;
-            st.push(d);
+            ans += d*place;
+            place*=10;
             sum+=d;
         }
 
-        while(!st.empty()){
-            ans = ans*10 + st.top();
-            st.pop();
-        }
+        // while(!st.empty()){
+        //     ans = ans*10 + st.top();
+        //     st.pop();
+        // }
 
         return sum*ans;
     }
