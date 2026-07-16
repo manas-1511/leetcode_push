da7c70ba2ff630e0c51fc77314f9bb6f9be235cc
manas-1511 <manas.p1511@gmail.com>
2026-07-16 10:54:20 +0530

    Time: 112 ms (5.09%) | Memory: 62.9 MB (5.03%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,40 +3,43 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> dis(m , vector<int>(n , 0));
-        vector<vector<int>> visited(m , vector<int>(n , 0));
-        queue<vector<int>> q;
+        vector<vector<int>> dis(m , vector<int>(n , INT_MAX));
+        vector<vector<int>> vis(m , vector<int>(n , 0));
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+                    queue<vector<int>> q;
+        for(int i =0 ; i <m ; i ++){
+            for(int j = 0 ; j< n ; j++){
                 if(mat[i][j] == 0){
-                    visited[i][j] = 1;
-
+                    vis[i][j] = 1;
+                    dis[i][j] = 0;
                     q.push({i , j , 0});
+
                 }
             }
         }
-
+            vector<int> dr = {0, 0 , -1 , 1};
+            vector<int> dc = {1 , -1 , 0 , 0};
         while(!q.empty()){
-            int sri = q.front()[0];
-            int srj = q.front()[1];
-            int d = q.front()[2];
-            dis[sri][srj] = d;
+            auto temp = q.front();
+            int r = temp[0];
+            int c = temp[1];
+            int d = temp[2];
             q.pop();
+
             
-            //right , down , left  , up
-            vector<int> dr = {0 , 1 , 0 , -1};
-            vector<int> dc = {1 , 0 , -1 ,0};
+            for(int i = 0 ; i < 4 ; i++){
+                int nr  = r+dr[i];
+                int nc = c+dc[i];
 
-            for(int i = 0 ; i < 4 ; i++ ){
-                int next_i = sri+dr[i];
-                int next_j = srj + dc[i];
+            if(nr >=0 && nc>=0 && nr<m && nc < n  && d+1 < dis[nr][nc]){
+                
+                dis[nr][nc] = d+1;
+                q.push({nr , nc , d+1});
 
-                if(next_i < m && next_j < n && next_i >= 0 && next_j >=0 && !visited[next_i][next_j] ){
-                    visited[next_i][next_j] = 1;
-                    q.push({next_i , next_j , d+1});
-                }
             }
+            }
+
+
         }
 
         return dis;
