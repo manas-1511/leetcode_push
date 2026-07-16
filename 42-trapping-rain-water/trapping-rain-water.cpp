74a77ff7b0e9572e542eab07155f6c411c613a8f
manas-1511 <manas.p1511@gmail.com>
2026-07-16 06:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (17.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        //TC O(n)
-        //SC O(1)
-        int n = arr.size();
-        int l = 0 ; 
-        int r= n-1 ;
-        int lmax = 0 , rmax = 0 , total = 0 ;
-        while(l<r){
-            if(arr[l] <= arr[r]){
-                if(lmax > arr[l]){
-                    total+=lmax-arr[l];
-                }
+    int trap(vector<int>& nums) {
+        //T(3N)
+        //S = O(2N)
+        int n = nums.size();
+        if(n==1) return 0;
+        vector<int> prefix(n , 0);
+        vector<int> suffix( n , 0 );
+        prefix[0] = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            prefix[i] = max(nums[i] , prefix[i-1]);
+        }
 
-                else{
-                    //arr[l] > lmax then update it
-                    lmax = arr[l];
-                }
-                l++;
-            }
+        suffix[n-1] = nums[n-1];
+        for(int i = n-2 ; i >= 0 ; i--){
+            suffix[i] = max(nums[i] , suffix[i+1]);
+        }
+        int total  = 0;
 
-            else{
-                //now the arr[r] > arr[l] i have to decrease r-- and before moving have to check 
-                //whether that cell can contain water or not just by checking the right side 
-                //not the left side bcz the left side has already larger or equal walls than rmax
-                //since we are only shrinking the window form the side having lower heights  
-                //if rcurr < rmax then the water can be stored 
-                if(rmax > arr[r]) total+=rmax-arr[r];
-                else rmax = arr[r];
-
-                r--;
+            //leftMax , rightMax 
+        for(int i = 0 ; i < n ; i++){
+            if(min(prefix[i] , suffix[i]) - nums[i] > 0){
+                total+=min(prefix[i] , suffix[i]) - nums[i];
             }
         }
 
         return total;
-
-
     }
 };
