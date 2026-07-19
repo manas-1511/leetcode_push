8529a0bc76b6c7ed5b0f31c561893dc57987b54f
manas-1511 <manas.p1511@gmail.com>
2026-07-19 10:21:58 +0530

    Time: 86 ms (36.33%) | Memory: 75 MB (90.23%) - LeetSync

---
+++ 3993-transform-binary-string-using-subsequence-sort/transform-binary-string-using-subsequence-sort.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    vector<bool> transformStr(string s, vector<string>& strs) {
+        int cnt1 = 0 ;
+        int cnt0 = 0;
+        int n = s.size();
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '0') cnt0++;
+            else cnt1++;
+        }
+            vector<bool> ans;
+        
+        for(auto& str:strs){
+            int c1 = 0;
+            int c0 = 0 ;
+            int c2  =0;
+            bool flag = true;
+            for(int i = 0 ; i< str.size() ; i++){
+                if(str[i] == '1') c1++;
+                else if(str[i] == '0') c0++;
+                else c2++;
+            }
+
+            if(c1 > cnt1){
+                ans.push_back(false);
+                continue;
+            }
+
+            if(c0 > cnt0){
+                ans.push_back(false);
+                continue;
+            }
+
+            for(int i = 0 ; i<n ; i++){
+                if(str[i] == '?'){
+                    if(c0 < cnt0){
+                        str[i] = '0';
+                        c0++;
+                    }
+
+                    else if(c1 < cnt1){
+                        str[i] = '1';
+                        c1++;
+                    }
+                }
+
+                
+
+                
+
+                
+            }
+            
+           
+
+            int buffer = 0;
+            bool f = true;
+            for(int i = 0 ; i < n ; i++){
+                if(s[i] == '1' && str[i] == '0') buffer++;
+                else if(s[i] == '0' && str[i] == '1')  buffer--;
+                if(buffer<0){
+                    f = false;
+                    break;
+                }
+            }
+            
+             if(f) ans.push_back(true);
+            else ans.push_back(false);
+
+            
+
+            
+        }
+
+        return ans;
+    }
+};
