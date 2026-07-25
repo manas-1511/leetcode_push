62acf1592f334c41213939104babe03caeadee9b
manas-1511 <manas.p1511@gmail.com>
2026-07-25 16:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (63.15%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -74,20 +74,35 @@ public:
             maxRow = max(it[0] , maxRow);
             maxCol =max(it[1] , maxCol);
         }
-            unordered_map<int , int > stoneNode;
+            // unordered_map<int , int > stoneNode;  //just to maintain which rows and cols have stones
+            //because the node(row or col here) having no stones will still represent a independent node 
+            // total number of rows = maxRow+1 and cols = maxCol+1
         DisjointUnion ds(maxRow+maxCol+2);
+       
         for(auto& it : stones){
             int row = it[0];
             int col = maxRow+it[1]+1;
-            ds.unionBySize(row , col);
-            stoneNode[row] = 1;
-            stoneNode[col] = 1;
+            ds.unionByRank(row , col);
+            // stoneNode[row] = 1;
+            // stoneNode[col] = 1;
 
         }
 
         int comp = 0;
-        for(auto& it : stoneNode){
-            if(ds.parent[it.first] == it.first) comp++;
+        //M1 using unordered_map to find the actual comp
+        // for(auto& it : stoneNode){
+        //     if(ds.parent[it.first] == it.first) comp++;
+        // }
+
+        //another way to check the nodes(i.e. rows/cols which dont contain any stones)
+        //will have the size == 1 since each of the node is paired/ grouped with some other nodes
+        //since a stone is represented by (i,j) and therefore should be paired with other node 
+        //if not paired then it is a no stone cell and dosenot represent a individual componenent
+
+
+        //m2 to find the actual comp 
+        for(int i = 0 ; i < maxRow+maxCol+2; i++){
+            if(ds.parent[i] == i && ds.rank[i] > 0) comp++;
         }
 
         return n-comp;
