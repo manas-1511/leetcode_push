42ffdde3f0e8e266ad63f092b317022ed2645a01
manas-1511 <manas.p1511@gmail.com>
2026-07-26 22:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (76.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        // int pos = 0;
-        // int neg = 0 ;
-        // int zero = 0 ;
         int n = nums.size();
-        // if(n== 3) return nums[0]*nums[1]*nums[2];
-        // for(int i = 0 ; i < nums.size() ; i++){
-        //     if(nums[i] < 0) neg++;
-        //     else if(nums[i] > 0 ) pos ++;
-        //     else zero++;
-        // }
-        sort(nums.begin() , nums.end());
-        // if(pos == 0) return nums[n-1]*nums[n-2]*nums[n-3];
-        // if(pos == 0 && zero >0) return 0;
-        // if(pos==1) return nums[n-1]*nums[0]*nums[1];
-        // if(pos==2) return nums[0]*nums[1]*nums[n-1];
-        return max( nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]);
+        int min1 = INT_MAX , min2 = INT_MAX , max1 = INT_MIN, max2 = INT_MIN , max3 = INT_MIN;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] <= min1){
+                min2 = min1 ;
+                min1 = nums[i];
+            }
+            else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+
+            if(nums[i]>=max1){
+                max3 = max2;
+                max2 = max1;
+                max1= nums[i];
+            }
+
+            else if(nums[i]>=max2){
+                max3 = max2;
+                max2 = nums[i];
+            }
+
+            else if(nums[i] > max3){
+                max3 = nums[i];
+            }
+
+
+        }
+
+        return max(min1*min2*max1 , max1*max2*max3);
 
     }
 };
