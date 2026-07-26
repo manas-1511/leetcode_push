2c356f944adbfa6ae958010186137c5ea541389c
manas-1511 <manas.p1511@gmail.com>
2026-07-26 15:03:56 +0530

    Time: 12 ms (30.76%) | Memory: 31.6 MB (10.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -12,10 +12,10 @@ public:
             else zero++;
         }
         sort(nums.begin() , nums.end());
-        if(pos == 0) return nums[n-1]*nums[n-2]*nums[n-3];
-        if(pos == 0 && zero >0) return 0;
-        if(pos==1) return nums[n-1]*nums[0]*nums[1];
-        if(pos==2) return nums[0]*nums[1]*nums[n-1];
+        // if(pos == 0) return nums[n-1]*nums[n-2]*nums[n-3];
+        // if(pos == 0 && zero >0) return 0;
+        // if(pos==1) return nums[n-1]*nums[0]*nums[1];
+        // if(pos==2) return nums[0]*nums[1]*nums[n-1];
         return max( nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]);
 
     }
