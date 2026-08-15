fa6d2322624fafd9e88a77b55eaf39e104e883a9
manas-1511 <manas.p1511@gmail.com>
2026-08-15 15:10:45 +0530

    Time: 28 ms (44.36%) | Memory: 27.4 MB (55.63%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int longestCommonSubsequence(string s, string t) {
-         int m = s.size();
-       int n = t.size();
-       vector<vector<int>> dp(m+1 , vector<int>(n+1));
-       for(int i = 0 ; i < m ; i ++){
-        for(int j = 0 ; j < n ; j++){
-            if(s[i] == t[j]) dp[i+1][j+1] = 1+dp[i][j];
-            else dp[i+1][j+1] = max(dp[i][j+1] , dp[i+1][j]);
-        }
-       }
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>> dp( m+1 , vector<int>(n+1));
+        for(int i = 0 ; i < m; i ++){
+            for(int j = 0 ; j < n ; j++){
+                if(s[i] == t[j]){
+                    dp[i+1][j+1] = 1+dp[i][j];
+                }
 
-       return dp[m][n];
+                else{
+                    dp[i+1][j+1] = max(dp[i][j+1] , dp[i+1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
